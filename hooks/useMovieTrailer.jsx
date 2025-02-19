282e56b5044b494531f9488077663c6f3888a366
piyushkrsingh07 <piyush23153101@akgec.ac.in>
2025-02-19 13:41:20 +0530

    main container done

--- hooks/useMovieTrailer.jsx
+++ hooks/useMovieTrailer.jsx
@@ -23,7 +23,7 @@ const useMovieTrailer=(movieId)=>{
         // const data=await fetch('https://api.themoviedb.org/3/movie/912649/videos?language=en-US',API_OPTIONS)
         //phle movieid hard coded tha
         const json=await data.json();
-        console.log(json)
+        console.log(json,"dekho trailer data")
 
         const filterData=json.results.filter(video=>video.type === "Trailer")
        
